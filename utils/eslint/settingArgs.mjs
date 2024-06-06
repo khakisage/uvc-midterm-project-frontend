9294d4f0f39052a105ab81d46cc0bc02cf048be7
khakisage <jongjin201@gmail.com>
2024-06-06 13:27:17 +0900

    📦 라이브러리 설치 및 설정 변경

--- utils/eslint/settingArgs.mjs
+++ utils/eslint/settingArgs.mjs
@@ -48,14 +48,20 @@ const settingArgs = {
       }
     ],
     'no-shadow': 0,
+    'import/no-unresolved': 0,
+    'react/prop-types': 0,
     'react/jsx-no-bind': 0,
+    'object-curly-newline': 0,
     'react/button-has-type': 0,
     'react/self-closing-comp': 0,
-    'react/react-in-jsx-scope': 0,
-    'react/jsx-one-expression-per-line': 0,
     'import/no-absolute-path': 0,
+    'react/react-in-jsx-scope': 0,
     'import/no-named-as-default': 0,
-    'import/no-named-as-default-member': 0
+    'tailwindcss/classnames-order': 0,
+    'tailwindcss/no-custom-classname': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'import/no-named-as-default-member': 0,
+    'tailwindcss/no-contradicting-classname': 0
   }
 };
 
